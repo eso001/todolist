079cecc6c37974a992e1464c811ea83d05118100
Eric <eso001@ucr.edu>
2016-01-20 17:47:30 -0800

    Complete Todo list and about page

--- SUPER.js
+++ SUPER.js
@@ -1,16 +1,48 @@
 
+
+
+
 var makeEvent = function(){
-var field = $('#addStuff').find('input[type="text"]').val();
-var name = field.value;
-if (name == " ") {
+var name = $('#addStuff').find('input[type="text"]').val();
+var listItem = $("<li class='lister'></li>")
+var editButton = $("<button class='edit button' type='button'>edit</button>");
+var deleteButton = $("<input type='button' class='delete button' value='x'>");
+var $name = listItem.text(name);
+if (name == "") {
     alert("What the Fuck is This");
 } else {
- $('#myList').text("<li>name</li>");
+ $name.appendTo($('#myList'));
+ editButton.appendTo($name);
+ deleteButton.appendTo($name);
 }
 
 };
+var clearValue = function() {
+ $('#addStuff').find('input[type="text"]').val("");
 
+};
 
-$('input[type=submit').click(function(){
-alert("Hi");
+var deleter = function() {
+$(this).closest('.lister').remove();
+
+};
+$(document).ready( function() {
+
+$('.buttonZORD').on('click', function(){
+makeEvent();
+clearValue();
+return false;
+});
+ $('#myList').on('click', "input.delete", function(){
+$(this).closest('.lister').remove();
+ });
+ $('#myList').on('click',"button.edit", function(){
+var change = prompt("What would you like to change this entry to?");
+var $name = $(this).closest('.lister').text(change);
+var editButton = $("<button class='edit button' type='button'>edit</button>");
+var deleteButton = $("<input type='button' class='delete button' value='x'>");
+ editButton.appendTo($name);
+ deleteButton.appendTo($name);
+
+ });
 });
